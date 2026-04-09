1f4b988cdb0ad8ea12e15359dff89006a2182de6
hasanawida <hasanawida123h@gmail.com>
2026-04-09 14:43:41 +0300

    add middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { NextResponse, type NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from "next/server"
 
 export function middleware(request: NextRequest) {
   return NextResponse.next()
@@ -6,6 +6,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 }

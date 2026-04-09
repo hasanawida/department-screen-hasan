26a3916de3353b2067c9517b6dd9d7b3a06fee82
hasanawida <hasanawida123h@gmail.com>
2026-04-09 13:24:20 +0300

    ts-nocheck orientation

--- app/orientation/[token]/page.tsx
+++ app/orientation/[token]/page.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createClient } from "@supabase/supabase-js";
 import { notFound } from "next/navigation";
